cdd437f68bc05bb5bbf4d37adfca37a5442fb428
njj-cmu <f.neiljohn.jomaya@cmu.edu.ph>
2024-09-10 08:12:22 +0800

    Super Final Copy

--- test.py
+++ test.py
@@ -6,9 +6,12 @@ class ArithmeticTest(unittest.TestCase):
     
     def test_add2(self):
         # Make it so that this line should not return an error
+        # Do not change the values being supplied.
         self.assertEqual(1.05 + 1.05, add(1.05, 1.05))
 
     # Add additional tests for subtract, multiply, and divide
+
+# Probably add new functions?
 
 def add(x : int, y : int) -> int:
     assert type(x) == "int"
